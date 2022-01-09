7d614dad6182ee2b4a7f90e97de032a5e6db7247
st140101 <siyana.todorova01@gmail.com>
2022-01-09 17:11:29 +0200

    added game functionality

--- Memory Game/game.js
+++ Memory Game/game.js
@@ -16,20 +16,178 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const database = getDatabase(app);
-//const dbRef = ref(database, "users");
 const lstorage = window.localStorage;
-console.log(lstorage);
-
 const currUser = lstorage.getItem("user");
-console.log(currUser);
+//TODO: ADD MODAL, CONNECT TO FIREBASE
+//------------------------------------------
+const deck = document.getElementById("deck");
+const modal = document.getElementById("modal");
 
-const dbRef = ref(getDatabase());
-get(child(dbRef, `users/${currUser}`)).then((snapshot) => {
-  if (snapshot.exists()) {
-    console.log(snapshot.val());
-  } else {
-    console.log("No data available");
-  }
-}).catch((error) => {
-  console.error(error);
+const start = document.getElementById("start-btn");
+const restart = document.getElementById("reset-btn");
+
+const timeCounter = document.getElementById("timer");
+let time;
+let minutes = 0;
+let seconds = 0;
+let timeStart = false;
+
+let opened = [];
+let matched = [];
+let cardArr = [
+  "elen.jpg", "elen.jpg",
+  "fox.jpg","fox.jpg",
+  "compass.jpg","compass.jpg",
+  "lion.jpg","lion.jpg",
+  "owl.jpg","owl.jpg",
+  "skull.jpg","skull.jpg",
+  "triangle.jpg","triangle.jpg",
+  "unicorn.jpg","unicorn.jpg",
+];
+
+
+//---START AND END GAME----
+function startGame() {
+  console.log("STARTED");
+	const shuffledDeck = shuffle(cardArr);
+	for (let i = 0; i < shuffledDeck.length; i++) {
+		
+		const li = document.createElement('li');
+		li.classList.add('card');
+		const img = document.createElement("img");
+		li.appendChild(img);
+		img.setAttribute("src", `./images/${shuffledDeck[i]}`);
+   //https://github.com/st140101/FMI-FrontEnd-2021/tree/master/Memory%20Game/images/+ "?raw=true"
+		deck.appendChild(li);
+	}
+}
+//startGame();
+
+const winGame = () => {
+	if (matched.length === 16) {
+		stopTimer();
+		//AddStats();
+		//displayModal();
+	}
+}
+//----------------------------
+
+deck.addEventListener("click", function(e) {	
+	console.log(e.target + " Was clicked");
+
+	flip(e.target);	
 });
+
+const flip = (card) => {
+  card.classList.add("flip");
+  console.log("FLIPPED");
+  addToOpened(card);
+}
+ 
+const addToOpened = (card) => {
+  if (opened.length === 0 || opened.length === 1) {
+    opened.push(card.firstElementChild);
+  }
+  compareCards();
+}
+
+const compareCards = () => {
+	if (opened.length === 2) {
+  		document.body.style.pointerEvents = "none";
+  }
+
+	if (opened.length === 2 && opened[0].src === opened[1].src) {
+		matchedCards();
+	} else if (opened.length === 2 && opened[0].src != opened[1].src) {
+		notMatched();
+	}
+}
+
+const matchedCards = () => {
+	setTimeout(function() {
+		opened[0].parentElement.classList.add("match");
+		opened[1].parentElement.classList.add("match");
+		matched.push(...opened);
+		document.body.style.pointerEvents = "auto";
+		winGame();
+		opened = [];
+	}, 600);
+}
+
+const notMatched = () => {
+	setTimeout(function() {
+		opened[0].parentElement.classList.remove("flip");
+		opened[1].parentElement.classList.remove("flip");
+		document.body.style.pointerEvents = "auto";
+		opened = [];
+	}, 600);
+}
+
+//---SHUFFLE----
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+//-----RESET-----
+const clearDeck = () => { //helper
+	while (deck.hasChildNodes()) {
+		deck.removeChild(deck.firstChild);
+	}
+}
+function reset() {
+  console.log("RESET");
+	stopTimer();
+	timeStart = false;
+	seconds = 0;
+	minutes = 0;
+  timeCounter.innerHTML = `Time: 0min 0s` ;
+	
+	matched = [];
+	opened = [];
+
+	clearDeck();
+	startGame();
+}
+//-------------------------------
+
+//-----TIMER-------
+const updateTimer = () => {
+	time = setInterval(function() {
+		seconds++;
+			if (seconds === 60) {
+				minutes++;
+				seconds = 0;
+			}
+		timeCounter.innerHTML = `Time: ${minutes}min ${seconds}s` ;
+	}, 1000);
+}
+
+const stopTimer = () => {
+  clearInterval(time);
+}
+//-----------------
+
+
+
+start.addEventListener('click',(e) => {
+  if (timeStart === false) {
+    timeStart = true; 
+    updateTimer();
+  }
+  console.log(e.target);
+  startGame();
+} );
+
+restart.addEventListener('click',(e) => {
+  console.log(e.target);
+  reset();
+} );
